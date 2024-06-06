c8aae3845cc9ddf68ce4feebdb8bec6a55fe5f46
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-06 15:13:22 +0200

    ESERCIZIO TERMINATO - codice js che stampa in console

--- js/main.js
+++ js/main.js
@@ -2,5 +2,18 @@
 
 // stampare in console numeri da 1 a 100
 for(let i=1; i<=100; i++){
-    console.log(i);
+
+    // console.log(i);
+
+    // se la divisione per 3 mi sa resto 0 e anche la divisione per 5 mi da resto 0 -> output: FizzBuzz
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0){ // altrimenti se solo la divisione per 3 mi da resto 0 -> output: fizz
+        console.log("Fizz");
+    } else if (i % 5 === 0){ // altrimenti se solo la divisione per 5 mi da resto 0 -> output: buzz
+        console.log("Buzz");
+    } else { // altrimenti -> output: i (che sarebbe il numero)
+        console.log(i);
+    }
+
 }
